2615c4ae90e45abb574c61e74a52a56d28807d55
ross <ross@doocom.cn>
2023-12-13 18:09:28 +0800

    fix: 使用内敛变量（123）手法内敛play变量

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,27 +1,3 @@
-function amountFor(aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience -30);
-      }
-      break;
-    case "comedy": 
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience -20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  } 
-
-  return result;
-}
-
-
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -30,6 +6,34 @@ function statement(invoice, plays) {
   const format = new Intl.NumberFormat("en-US",
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format;
+
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+
+  function amountFor(aPerformance, play) {
+    let result = 0;
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience -30);
+        }
+        break;
+      case "comedy": 
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience -20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    } 
+  
+    return result;
+  }
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
