125eddb8ffb1e69ede2b9bb584c8d936ba67563b
ross <ross@doocom.cn>
2023-12-14 20:56:02 +0800

    fix: 替代renderPlainText中的invoice，改用data

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,10 +1,11 @@
 function statement(invoice, plays) {
   const statementData = {};
+  statementData.customer = invoice.customer;
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `Statement for ${data.customer}\n`;
 
   function usd(aNumber) {
     // Intl.NumberFormat 用於格式化數字，將其轉換為貨幣格式-可以設置為各個國家的貨幣格式
