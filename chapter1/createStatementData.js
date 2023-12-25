5b31a0df788a0f7431797c0b9e3f6d3ffd0517c2
ICE-XIAOWU <zhucelehaojiu@gmail.com>
2023-12-26 00:17:57 +0800

    feat: 处理Performance类中volumeCredits的计算

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -9,10 +9,7 @@ class PerformancesCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -37,6 +34,10 @@ class ComedyCalculator extends PerformancesCalculator {
     result += 300 * this.performance.audience;
     
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
