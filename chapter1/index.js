a06a3d895d0695400c661417393da4fb7d89b869
ICE-XIAOWU <zhucelehaojiu@gmail.com>
2023-12-25 23:44:30 +0800

    feat: 添加HTML类型的模板输出

--- chapter1/index.js
+++ chapter1/index.js
@@ -9,15 +9,6 @@ function statement(invoice, plays) {
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
-  function usd(aNumber) {
-    // Intl.NumberFormat 用於格式化數字，將其轉換為貨幣格式-可以設置為各個國家的貨幣格式
-    return new Intl.NumberFormat("en-US",{ 
-      style: "currency", 
-      currency: "USD",
-      minimumFractionDigits: 2, 
-    }).format(aNumber / 100);
-  }
-
   for (let perf of data.performances) {
     // print line for this order
     result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
@@ -28,4 +19,33 @@ function renderPlainText(data) {
   return result;
 }
 
-console.log(statement(invoices[0], plays))
+function usd(aNumber) {
+  // Intl.NumberFormat 用於格式化數字，將其轉換為貨幣格式-可以設置為各個國家的貨幣格式
+  return new Intl.NumberFormat("en-US",{ 
+    style: "currency", 
+    currency: "USD",
+    minimumFractionDigits: 2, 
+  }).format(aNumber / 100);
+}
+
+// HTML版本账单
+function htmlStatement(invoices, plays) {
+  return renderHtml(createStatementData(invoices, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+
+  result += `<table>\n`;
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+  for (let perf of data.performances) {
+    result += `<td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+console.log(htmlStatement(invoices[0], plays));
